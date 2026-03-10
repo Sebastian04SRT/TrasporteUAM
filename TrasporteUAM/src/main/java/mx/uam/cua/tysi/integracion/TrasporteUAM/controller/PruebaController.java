665e4dbdc24dbad4aaaa9de24296ca6ccc2a4da5
Sebastian Robledo <sebastian.robledo@cua.uam.mx>
2026-03-10 13:45:36 -0600

    escribi mi controlador

---
+++ TrasporteUAM/src/main/java/mx/uam/cua/tysi/integracion/TrasporteUAM/controller/PruebaController.java
@@ -0,0 +1,19 @@
+package mx.uam.cua.tysi.integracion.TrasporteUAM.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PruebaController {
+
+    @GetMapping("/prueba")
+    public String prueba() {
+        return "Bienvenido a trasporte UAMg";
+    }
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "Bienvenido a trasporte UAM";
+    }
+}
